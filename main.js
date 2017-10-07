ea8b3cbe8257e8c32a9b55fec238628e7d0334bf
sgc88 <sehribancheng@yahoo.com>
2017-10-07 16:40:53 -0700

    set html and js file. keyboardevent is working

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  // console.log("check");
+  // $('#raceTrack').css('height','300');
+$('#race').keydown(function(){
+
+  console.log('keyboard');
+
+});
+
+
+
+
+});
