15e875b35d46198d870604b65f79f5f763f42a90
sgc88 <sehribancheng@yahoo.com>
2017-10-07 18:11:17 -0700

    set up who is winner

--- main.js
+++ main.js
@@ -13,20 +13,30 @@ $(document).ready(function(){
     // event.which to find out the key code
     // specifiying a key for each car(let say this is car1)
     if(event.which === 81){
-      console.log("clicked 81");
-      $('#raceTrack1').css('margin-left', speed1);
-      speed1 += 10;
-
+        //now we need to set up who could be a winner
+      if(speed1 === 300){
+        alert("winner is car 1");
+      }else{
+        //no one wins? keep driving
+        console.log("clicked 81");
+        $('#raceTrack1').css('margin-left', speed1);
+        speed1 += 10;
+      }
     }
     //specifiyin a key for cars(let say this is car2)
     if(event.which === 80){
-      console.log("clicked 80");
-      $("#raceTrack2").css('margin-left', speed2);
-      speed2 += 10;
+      //now we need to set up who could be a winner2
+      if(speed2 === 300){
+        alert("the winner is car2");
+        // no one wins? keep driving
+      }else{
+        console.log("clicked 80");
+        $("#raceTrack2").css('margin-left', speed2);
+        speed2 += 10;
 
+      }
 
     }
-
 
   });
 
