5c4516add427c701685bd1fd47c4cd68b73d5646
sgc88 <sehribancheng@yahoo.com>
2017-10-07 17:38:46 -0700

    can move both cars

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
+var speed1=0;
+var speed2=0;
+
 $(document).ready(function(){
 
   // $('#raceTrack').css('height','300');
@@ -10,11 +14,15 @@ $(document).ready(function(){
     // specifiying a key for each car(let say this is car1)
     if(event.which === 81){
       console.log("clicked 81");
+      $('#raceTrack1').css('margin-left', speed1);
+      speed1 += 10;
 
     }
     //specifiyin a key for cars(let say this is car2)
     if(event.which === 80){
       console.log("clicked 80");
+      $("#raceTrack2").css('margin-left', speed2);
+      speed2 += 10;
 
 
     }
