1a45e9c9ad8980c8786a9bce6b1a038c08fc74df
sgc88 <sehribancheng@yahoo.com>
2017-10-07 17:15:46 -0700

    specified a key for each car

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 $(document).ready(function(){
 
-  // console.log("check");
   // $('#raceTrack').css('height','300');
-$('#race').keydown(function(){
+  $('#race').keydown(function(event){
 
-  console.log('keyboard');
+    // find a way to only listen to keydown for 'Q' and 'P'
+    // 'Q' key is 81
+    // 'P' key is 80
+    // event.which to find out the key code
+    // specifiying a key for each car(let say this is car1)
+    if(event.which === 81){
+      console.log("clicked 81");
 
-});
+    }
+    //specifiyin a key for cars(let say this is car2)
+    if(event.which === 80){
+      console.log("clicked 80");
+
+
+    }
+
+
+  });
 
 
 
